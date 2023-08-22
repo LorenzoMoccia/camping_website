94fad0a5f0ee8eaf7f36a31397b5a55a5656b8ee
LorenzoMoccia <moccialorenzo98@gmail.com>
2023-08-22 23:01:22 +0200

    Delete booking

--- camping_backend/booking/urls.py
+++ camping_backend/booking/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ReservationFormView
+from .views import ReservationFormView, ReservationListView, DeleteReservationView
 
 urlpatterns = [
     path('make_reservation/', ReservationFormView.as_view(), name='make_reservation'),
+    path('reservation_list/', ReservationListView.as_view(), name='reservation_list'),
+    path('delete_reservation/<int:pk>/', DeleteReservationView.as_view(), name='delete_reservation'),
 ]
--- camping_backend/booking/views.py
+++ camping_backend/booking/views.py
@@ -2,6 +2,9 @@ from django.views.generic.edit import FormView
 from .forms import ReservationForm
 from .models import Booking
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.views.generic.base import View
 
 class ReservationFormView(FormView):
     form_class = ReservationForm
@@ -11,3 +14,21 @@ class ReservationFormView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+    
+class ReservationListView(View):
+    def get(self, request):
+        reservations = Booking.objects.all()
+        return render(request, 'booking/booking_list.html', {'reservations': reservations})
+
+    def post(self, request):
+        reservation_id = request.POST.get('reservation_id')
+        if reservation_id:
+            reservation = Booking.objects.get(pk=reservation_id)
+            reservation.delete()
+        return redirect('reservation_list')
+
+class DeleteReservationView(View):
+    def get(self, request, pk):
+        reservation = Booking.objects.get(pk=pk)
+        reservation.delete()
+        return redirect('reservation_list')
